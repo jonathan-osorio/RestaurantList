03bb3ba0fd7926cc7a1333b893b0b598a6db1a95
Jonathan Osório <jonathan.d.osorio@cgi.com>
2024-09-22 21:42:02 +0100

    - Creating Models and Data Context

--- RestaurantList/Program.cs
+++ RestaurantList/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<RestaurantListContext>(otions => optinos.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddDbContext<RestaurantListContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
