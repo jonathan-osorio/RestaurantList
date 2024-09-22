8f935465f4cff5bede79b11e35687bd83f08d8a2
Jonathan Osório <jonathan.d.osorio@cgi.com>
2024-09-22 21:53:03 +0100

    Creating Controller

---
+++ RestaurantList/Controllers/RestaurantList.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantList.Data;
+using RestaurantList.Models;
+
+namespace RestaurantList.Controllers
+{
+    public class RestaurantList : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
